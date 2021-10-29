e097bf9441d234105113a29c75ccc5babfcba06e
Daniel Valadas <info@danielvaladas.com>
2021-10-29 02:16:45 -0400

    Update Build.cs

--- build/Build.cs
+++ build/Build.cs
@@ -244,7 +244,6 @@ class Build : NukeBuild
       Git("rm -rf .");
       Git("checkout -b site origin/site"); // pulling a local copy of the current deployment.
       Git("status");
-      Git("rm -rf ."); // Delete all files before so we have a diff if something is no longer present in the new build.
       Git("status");
       Git("checkout deploy -- www"); // pulls only docs from our temporary deploy branch.
       Git("status");
