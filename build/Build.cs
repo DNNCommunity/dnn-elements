25a98998a5d9f6f27b5683308b5fae7c35f80118
Daniel Valadas <info@danielvaladas.com>
2021-10-29 00:39:59 -0400

    Trying to fix

--- build/Build.cs
+++ build/Build.cs
@@ -233,16 +233,15 @@ class Build : NukeBuild
     {
       Git("config --global user.name 'Daniel Valadas'");
       Git("config --global user.email 'info@danielvaladas.com'");
-      Git($"remote set-url origin https://{GithubToken}@github.com/{organizationName}/{repositoryName}.git");
+      Git($"remote set-url origin https://{organizationName}:{GithubToken}@github.com/{organizationName}/{repositoryName}.git");
       Git("status");
       Git("add www -f"); // Force adding because it is usually gitignored.
       Git("status");
       Git("commit --allow-empty -m \"Commit latest build\""); // We allow an empty commit in case the last change did not affect the site.
       Git("status");
-      Git("branch -D site");
-      Git("checkout -b origin/site"); // pulling a local copy of the current deployment.
+      Git("checkout -b site origin/site"); // pulling a local copy of the current deployment.
       Git("status");
-      Git("rm -rf ."); // Delete all files before so we have a diff if something is no longer present in the new build.
+      Git("rm -r ."); // Delete all files before so we have a diff if something is no longer present in the new build.
       Git("status");
       Git("checkout deploy -- www"); // pulls only docs from our temporary deploy branch.
       Git("status");
