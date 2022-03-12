58db9daf8d173634cc2e74db027ffa49facdf0b1
David Poindexter <dpoindexter@nvisionative.com>
2022-03-11 23:16:13 -0500

    Initial implementation of Storybook with one component (dnn-button)

---
+++ src/components/dnn-button/dnn-button.stories.ts
@@ -0,0 +1,18 @@
+export default {
+    // This creates a 'Components' folder and a 'dnn-element' subfolder
+    title: 'Compenents/dnn-button',
+};
+
+const Template = (args) => `<dnn-button type=${args.type} reversed=${args.reversed} size=${args.size} confirm=${args.confirm} confirmYesText=${args.confirmYesText} confirmNoText=${args.confirmNoText} confirmMessage=${args.confirmMessage} disabled=${args.disabled}>Click Me</dnn-button>`;
+
+export const Example = Template.bind({});
+Example.args = {
+    type: 'primary',
+    reversed: false,
+    size: 'medium',
+    confirm: false,
+    confirmYesText: 'Yes',
+    confirmNoText: 'No',
+    confirmMessage: 'Are you sure?',
+    disabled: false,
+};
