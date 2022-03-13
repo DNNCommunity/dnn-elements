4d1a4d34ceba8e73d853b193063bd2864626d5e0
David Poindexter <dpoindexter@nvisionative.com>
2022-03-13 19:31:19 -0400

    Update dnn-button story to use lit-html

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,33 @@
+import {
+  addParameters,
+  setCustomElements,
+} from '@storybook/web-components';
+import { BADGE } from '@geometricpanda/storybook-addon-badges';
+import {version} from '../package.json';
+
 import {defineCustomElements} from '../loader';
 
 defineCustomElements();
+
+addParameters({
+  badgesConfig: {
+    [BADGE.DEFAULT]: {
+      contrast: '#edeef7',
+      color: '#32394D',
+      title: version
+    },
+    [BADGE.STABLE]: {
+      contrast: '#00ab6d',
+      color: '#ffffff',
+      title: 'Stable'
+    },
+    [BADGE.BETA]: {
+      contrast: '#f8ab00',
+      color: '#32394D',
+      title: 'In Progress'
+    }
+  },
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,4 +37,8 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  badges: [BADGE.DEFAULT, BADGE.STABLE],
+  docs: {
+    inlineStories: true,
+  }
 }
--- src/components/dnn-button/dnn-button.stories.ts
+++ src/components/dnn-button/dnn-button.stories.ts
@@ -1,17 +1,77 @@
+import { html } from "lit-html";
+import { ifDefined } from 'lit-html/directives/if-defined';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+import { Meta } from "@storybook/web-components";
 import readme from './readme.md';
 
 export default {
-    title: 'Components/dnn-button',
+    title: 'Forms/Button',
     component: 'dnn-button',
     parameters: {
+        actions: {
+            handles: ['confirmed', 'canceled'],
+        },
         notes:  readme,
-    }
-};
+    },
+    argTypes: {
+        type: {
+          options: ['primary', 'secondary', 'tertiary'],
+          control: {
+            type: 'select',
+          },
+        },
+        reversed: {
+          control: 'boolean',
+        },
+        size: {
+            options: ['small', 'normal', 'large'],
+            control: {
+                type: 'select',
+            },
+        },
+        confirm: {
+            control: 'boolean',
+        },
+        confirmYesText: {
+            control: 'text',
+        },
+        confirmNoText: {
+            control: 'text',
+        },
+        disabled: {
+          control: 'boolean',
+        },
+      },
+      args: {
+        type: null,
+        reversed: null,
+        size: null,
+        confirm: null,
+        confirmYesText: null,
+        confirmNoText: null,
+        confirmMessage: null,
+        ' ': `Click Me!`,
+        disabled: null,
+      },    
+} as Meta;
 
-const Template = (args) => `<dnn-button type=${args.type} reversed=${args.reversed} size=${args.size} confirm=${args.confirm} confirmYesText=${args.confirmYesText} confirmNoText=${args.confirmNoText} confirmMessage=${args.confirmMessage} disabled=${args.disabled}>Click Me</dnn-button>`;
+const Template = (args) =>
+    html`
+        <dnn-button
+            type=${ifDefined(args.type)}
+            ?reversed=${ifDefined(args.reversed)}
+            size=${ifDefined(args.size)}
+            ?confirm=${ifDefined(args.confirm)}
+            confirmYesText=${ifDefined(args.confirmYesText)}
+            confirmNoText=${ifDefined(args.confirmNoText)}
+            ?confirmMessage=${ifDefined(args.confirmMessage)}
+            ?disabled=${ifDefined(args.disabled)}>
+            ${unsafeHTML(args[' '])}
+        </dnn-button>
+    `;
 
-export const Example = Template.bind({});
-Example.args = {
+export const Default = Template.bind({});
+Default.args = {
     type: 'primary',
     reversed: false,
     size: 'medium',
