607045ed00e4849504f06b0b8573403abbdd520c
Daniel <info@danielvaladas.com>
2022-03-19 15:06:49 -0400

    Adjusted the slot names in the permission grid

--- src/components/dnn-permissions-grid/dnn-permissions-grid.tsx
+++ src/components/dnn-permissions-grid/dnn-permissions-grid.tsx
@@ -154,10 +154,10 @@ export class DnnPermissionsGrid {
         checked={checked}
         onCheckedchange={e => this.handleRoleChanged(e.detail, rolePermission, permissionDefinition)}
       >
-        <div slot="intermediate">
+        <div slot="intermediateicon">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/></svg>
         </div>
-        <div slot="unchecked">
+        <div slot="uncheckedicon">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM4 12c0-4.42 3.58-8 8-8 1.85 0 3.55.63 4.9 1.69L5.69 16.9C4.63 15.55 4 13.85 4 12zm8 8c-1.85 0-3.55-.63-4.9-1.69L18.31 7.1C19.37 8.45 20 10.15 20 12c0 4.42-3.58 8-8 8z"/></svg>
         </div>
         <span>{permissionDefinition.permissionName}</span>
@@ -175,10 +175,10 @@ export class DnnPermissionsGrid {
         checked={checked}
         onCheckedchange={e => this.handleUserChanged(e.detail, userPermission, permissionDefinition)}
       >
-        <div slot="intermediate">
+        <div slot="intermediateicon">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/></svg>
         </div>
-        <div slot="unchecked">
+        <div slot="uncheckedicon">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM4 12c0-4.42 3.58-8 8-8 1.85 0 3.55.63 4.9 1.69L5.69 16.9C4.63 15.55 4 13.85 4 12zm8 8c-1.85 0-3.55-.63-4.9-1.69L18.31 7.1C19.37 8.45 20 10.15 20 12c0 4.42-3.58 8-8 8z"/></svg>
         </div>
         <span>{permissionDefinition.permissionName}</span>
