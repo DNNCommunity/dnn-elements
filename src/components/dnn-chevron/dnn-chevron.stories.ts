4bf7ec299a96cf86c2a85a93972b65969b7043fc
David Poindexter <dpoindexter@nvisionative.com>
2022-03-18 23:42:51 -0400

    Added story for dnn-chevron

---
+++ src/components/dnn-chevron/dnn-chevron.stories.ts
@@ -0,0 +1,42 @@
+import { html } from "lit-html";
+import { ifDefined } from 'lit-html/directives/if-defined';
+import { Meta } from "@storybook/web-components";
+import readme from './readme.md';
+
+export default {
+    title: 'Elements/Chevron',
+    component: 'dnn-chevron',
+    parameters: {
+        actions: {
+            handles: ['changed'],
+        },
+        notes:  readme,
+    },
+    argTypes: {
+        'expandText': {
+            control: 'text',
+        },
+        'collapseText': {
+            control: 'text',
+        },
+        'expanded': {
+            control: 'boolean',
+          },
+        },
+} as Meta;
+
+const Template = (args) =>
+    html`
+        <dnn-chevron
+            expandText=${ifDefined(args.expandText)}
+            collapseText=${ifDefined(args.collapseText)}
+            ?expanded=${ifDefined(args.expanded)}>
+        </dnn-chevron>
+    `;
+
+export const Chevron = Template.bind({});
+Chevron.args = {
+    expandText: 'expand',
+    collapseText: 'collapse',
+    expanded: false,
+};
