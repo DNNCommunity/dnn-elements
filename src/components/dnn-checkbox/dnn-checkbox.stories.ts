126c5fd75fdf32ca94db49784ec80b5f7f591c23
David Poindexter <dpoindexter@nvisionative.com>
2022-03-18 23:33:15 -0400

    Added story for dnn-checkbox

---
+++ src/components/dnn-checkbox/dnn-checkbox.stories.ts
@@ -0,0 +1,43 @@
+import { html } from "lit-html";
+import { ifDefined } from 'lit-html/directives/if-defined';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+import { Meta } from "@storybook/web-components";
+import readme from './readme.md';
+
+export default {
+    title: 'Elements/Checkbox',
+    component: 'dnn-checkbox',
+    parameters: {
+        actions: {
+            handles: ['checkedchange'],
+        },
+        notes:  readme,
+    },
+    argTypes: {
+        'checked': {
+            control: 'boolean',
+        },
+        'useIntermediate': {
+            control: 'boolean',
+        },
+        'value': {
+            control: 'text',
+        },
+      },
+} as Meta;
+
+const Template = (args) =>
+    html`
+        <dnn-checkbox
+            ?checked=${ifDefined(args.checked)}
+            ?useIntermediate=${ifDefined(args.useIntermediate)}
+            value=${ifDefined(args.value)}
+        </dnn-checkbox>
+    `;
+
+export const Checkbox = Template.bind({});
+Checkbox.args = {
+    checked: false,
+    useIntermediate: false,
+    value: '1',
+};
