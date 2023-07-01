42c7b7e83e74a42100da7bcf43bdcaa8cd693cdf
Daniel Valadas <info@danielvaladas.com>
2023-07-01 19:52:06 -0400

    Update packages/stencil-library/src/components/dnn-select/dnn-select.tsx

--- packages/stencil-library/src/components/dnn-select/dnn-select.tsx
+++ packages/stencil-library/src/components/dnn-select/dnn-select.tsx
@@ -49,7 +49,7 @@ export class DnnSelect {
   }
 
   componentDidLoad() {
-    this.aplySlottedItemsToSelect();
+    this.applySlottedItemsToSelect();
   }
 
   private applySlottedItemsToSelect () {
