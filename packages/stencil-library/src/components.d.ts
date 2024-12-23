29a8b2e9a03bc38a1b8e0bc4df9321abacab7b75
Daniel <info@danielvaladas.com>
2024-12-23 00:00:11 -0500

    Changed default order of checkboxes in dnn-permission-grid
    Related to https://github.com/dnnsoftware/Dnn.Platform/issues/6214
    
    And closes #1240

--- packages/stencil-library/src/components.d.ts
+++ packages/stencil-library/src/components.d.ts
@@ -142,7 +142,7 @@ export namespace Components {
          */
         "name": string;
         /**
-          * A function that will be called when the checkbox changes its checked state. Can be used to customize the order of the states when the component is clicked. Only called if you also use the tri-state feature (useIntermediate).
+          * A function that will be called when the checkbox changes needs to change and returns the next state. Can be used to customize the order of the states when the component is clicked. Only called if you also use the tri-state feature (useIntermediate).
          */
         "nextStateHandler": (currentState: CheckedState) => CheckedState;
         /**
@@ -1440,7 +1440,7 @@ declare namespace LocalJSX {
          */
         "name"?: string;
         /**
-          * A function that will be called when the checkbox changes its checked state. Can be used to customize the order of the states when the component is clicked. Only called if you also use the tri-state feature (useIntermediate).
+          * A function that will be called when the checkbox changes needs to change and returns the next state. Can be used to customize the order of the states when the component is clicked. Only called if you also use the tri-state feature (useIntermediate).
          */
         "nextStateHandler"?: (currentState: CheckedState) => CheckedState;
         /**
