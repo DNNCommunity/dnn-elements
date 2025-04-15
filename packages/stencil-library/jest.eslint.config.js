93c1f497276ddc8e825fdd7a263781e5413ae57d
Daniel <info@danielvaladas.com>
2025-04-14 20:41:44 -0400

    Basics working

---
+++ packages/stencil-library/jest.eslint.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["<rootDir>/src/eslint-plugin/__tests__/**/*.tests.ts"],
+}
---
+++ packages/stencil-library/src/eslint-plugin/__tests__/no-label-slot-in-checkbox.tests.ts
@@ -0,0 +1,27 @@
+import { RuleTester } from "eslint";
+import { noLabelSlotInCheckbox } from "../src/rules/no-label-slot-in-checkbox";
+const tsParser = require.resolve("@typescript-eslint/parser");
+
+const ruleTester = new RuleTester({
+    parser: tsParser,
+    parserOptions: {
+        ecmaVersion: 2022,
+        ecmaFeatures: {
+            jsx: true,
+        },
+        sourceType: "module",
+    },
+});
+
+ruleTester.run("no-label-slot-in-checkbox", noLabelSlotInCheckbox, {
+    valid: [
+        { code: "<dnn-checkbox></dnn-checkbox>" },
+    ],
+    invalid: [
+        {
+            code: "<dnn-checkbox onClick={e => console.log(e)}><label>Label Text</label></dnn-checkbox>",
+            errors: [{ messageId: "noLabelSlotInCheckbox" }],
+            output: "<label>\nLabel Text\n<dnn-checkbox onClick={e => console.log(e)}></dnn-checkbox>\n</label>",
+        },
+    ],
+});
