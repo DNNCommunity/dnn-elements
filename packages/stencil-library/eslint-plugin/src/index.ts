df1e0e0e41c64d2aebf8fb3922fdfdd2451ce780
Daniel <info@danielvaladas.com>
2025-04-16 22:36:58 -0400

    Added some metadata

--- packages/stencil-library/eslint-plugin/src/index.ts
+++ packages/stencil-library/eslint-plugin/src/index.ts
@@ -1,12 +1,20 @@
 import { rules } from "./rules";
 
+const { name, version } =
+  // `import`ing here would bypass the TSConfig's `"rootDir": "src"`
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require("../../package.json") as typeof import("../../package.json");
+
 const plugin = {
     configs: {
         get recommended() {
             return recommended;
-        }
-    }
-}
+        },
+    },
+    meta: { name, version },
+    rules,
+};
+
 const recommended = {
     plugins: {
         "dnn-elements": plugin,
