407c27db6c0bf2bb9154b4f1e27ac6b4c591a738
Daniel <info@danielvaladas.com>
2025-04-18 02:56:32 -0400

    First rule and test

---
+++ packages/stencil-library/eslint-plugin/src/rules/button-type-to-appearance.ts
@@ -0,0 +1,42 @@
+import { createRule } from "../utils";
+
+export const rule = createRule({
+    name: "button-type-to-appearance",
+    defaultOptions: [],
+    meta: {
+        docs: {
+            description: "type property deprecated in favor of appearance",
+            recommended: true,
+            url: "https://github.com/DNNCommunity/dnn-elements/releases/tag/v0.24.0",
+        },
+        type: "problem",
+        fixable: "code",
+        messages: {
+            buttonTypeToAppearance: "type property should be replaced with appearance."
+        },
+        schema: [],
+    },
+    create(context) {
+        return {
+            JSXElement(node) {
+                if (
+                    node.openingElement.name.type === "JSXIdentifier" &&
+                    node.openingElement.name.name === "dnn-button"
+                ) {
+                    for (const attr of node.openingElement.attributes) {
+                        if (
+                            attr.type === "JSXAttribute" &&
+                            attr.name.name === "type"
+                        ) {
+                            context.report({
+                                node: attr,
+                                messageId: "buttonTypeToAppearance",
+                                fix: (fixer) => fixer.replaceText(attr.name, "appearance"),
+                            });
+                        }
+                    }
+                }
+            }
+        };
+    },
+});
