e7229679db1e3e3728bc277a39986b6ce91a7c12
Daniel <info@danielvaladas.com>
2025-04-19 18:28:11 -0400

    Actually use the rules in the plugin
    New rules where created but they were not added to the plugin or documented

--- packages/stencil-library/eslint-plugin/src/rules/index.ts
+++ packages/stencil-library/eslint-plugin/src/rules/index.ts
@@ -1,5 +1,15 @@
+import { rule as buttonFormButtonTypeToType } from "./button-formButtonType-to-type.js";
+import { rule as dnnInputNoDisableValidityReporting } from "./dnn-input-no-disableValidityReporting.js";
+import { rule as dnnModalNoBackgroundDismiss} from "./dnn-modal-no-background-dismiss.js"
+import { rule as dnnSearchboxNoDebounced } from "./dnn-searchbox-no-debounced.js"
+import { rule as dnnSelectNoDisableValidityReporting} from "./dnn-select-no-disableValidityReporting.js"
 import { rule as noLabelSlotInCheckbox } from "./no-label-slot-in-checkbox.js";
 
 export const rules = {
+    "button-formButtonType-to-type": buttonFormButtonTypeToType,
+    "dnn-input-no-disableValidityReporting": dnnInputNoDisableValidityReporting,
+    "dnn-modal-no-background-dismiss": dnnModalNoBackgroundDismiss,
+    "dnn-searchbox-no-debounced": dnnSearchboxNoDebounced,
+    "dnn-select-no-disableValidityReporting": dnnSelectNoDisableValidityReporting,
     "no-label-slot-in-checkbox": noLabelSlotInCheckbox,
 }
