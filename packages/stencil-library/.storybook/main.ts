12ccc7e7d88e847ee607ea23b474850aa5f43a2f
Daniel <info@danielvaladas.com>
2025-06-11 20:29:33 -0400

    WIP: @stencil/storybook-plugin experimentation
    This is an experimentation of a new plugin maintained by the stencil team at https://github.com/stenciljs/storybook which is also a work in progress and not ready for prime time. However it currently builds and when complete will provide the following advantages.
    
    - Less config
    - Automatic and type-safe argTypes
    - Better automatic docs
    - Shorter stories (less config) and read TSX support for stories without using lit-html or other hacks

--- packages/stencil-library/.storybook/main.ts
+++ packages/stencil-library/.storybook/main.ts
@@ -1,25 +1,19 @@
-import type { StorybookConfig } from '@storybook/web-components-vite';
-import { join, dirname } from "path"
+import type { StorybookConfig } from "@stencil/storybook-plugin";
 
-/**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
-}
+
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/components/**/*.stories.tsx",
+    // "../src/**/*.mdx",
+    // "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-a11y'),
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
   ],
   "framework": {
-    "name": getAbsolutePath('@storybook/web-components-vite'),
-    "options": {}
+    "name": "@stencil/storybook-plugin",
   },
   staticDirs: [{ from: './assets', to: '/assets'}],
 };
--- packages/stencil-library/.storybook/preview.tsx
+++ packages/stencil-library/.storybook/preview.tsx
@@ -1,17 +1,9 @@
-// Reverting to .ts as React is no longer part of this version
-import type { Preview } from '@storybook/web-components';
-import { setCustomElementsManifest } from '@storybook/web-components';
-import { defineCustomElements } from '../loader';
-import customElements from '../custom-elements.json';
-import { Title, Subtitle, Description, Primary, Controls, Stories } from '@storybook/blocks';
-import React from 'react';
-import { h } from '@stencil/core';
-
-// Register Stencil components and set the custom elements manifest
-defineCustomElements();
-setCustomElementsManifest(customElements);
+import { type Preview } from "@stencil/storybook-plugin";
+import { Title, Subtitle, Description, Primary, Controls, Stories} from "@storybook/addon-docs/blocks";
+import { h } from "@stencil/core";
 
 const preview: Preview = {
+  tags: ["autodocs"],
   parameters: {
     controls: {
       matchers: {
@@ -19,21 +11,22 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-    tags: ["autodocs"],
+    actions: { argTypesRegex: "^on[A-Z].*" },
     docs: {
-      page: () => (
-        [
+      source: {
+        excludeDecorators: true,
+      },
+      page: () => [
           <Title />,
           <Subtitle />,
           <Primary />,
           <Controls />,
           <Stories />,
-          <Description />
-        ]
-      ),
+          <Description />,
+      ],
       extractComponentDescription: (component, { notes }) => {
         if (notes) {
-          return typeof notes === 'string' ? notes : notes.markdown || notes.text;
+          return typeof notes === "string" ? notes : notes.markdown || notes.text;
         }
         return null;
       },
